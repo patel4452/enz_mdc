2f71a994bcc802ed59cb6b04045dadbce1c61a90
Dushyant Patel <dushyant.engt@gmail.com>
2022-12-12 22:47:26 +0800

    section text changes

--- src/components/textanimate.jsx
+++ src/components/textanimate.jsx
@@ -7,9 +7,9 @@ class Sectiontext extends Component {
     return (
       <div className="section section-text">
         <Fade bottom>
-          <h2 className='copy-title title-primary'>LEARN HERE. THERE. EVERYWHERE.</h2>
-          <p className='copy-dark'>Start your future-focused New Zealand education journey with our new ways of learning. Explore our range of flexible study options and choose how you study.</p>
-          <p className='copy-dark'>Enjoy the convenience of a locally delivered course with <span className='underline'>global pathways</span>, or study from anywhere in the worls by <span className='underline'>learning online</span>. Learn about our high-quality education and unbeatable lifestyle when you <span className='underline'>study right here in New Zealand.</span></p>
+          <h2 className='copy-title title-primary pb-4'>LEARN HERE. THERE. EVERYWHERE.</h2>
+          <p className='copy-dark pb-2'>Start your future-focused New Zealand education journey with our new ways of learning. Explore our range of flexible study options and choose how you study.</p>
+          <p className='copy-dark pb-2'>Enjoy the convenience of a locally delivered course with <span className='underline'>global pathways</span>, or study from anywhere in the worls by <span className='underline'>learning online</span>. Learn about our high-quality education and unbeatable lifestyle when you <span className='underline'>study right here in New Zealand.</span></p>
         </Fade>
       </div>
     );
