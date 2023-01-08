a4141c8d1656600bbb8d13f512401fbde8c19b44
Dushyant Patel <dushyant.engt@gmail.com>
2023-01-08 20:04:31 +0800

    slider testing

--- src/components/Smoothscroll.jsx
+++ src/components/Smoothscroll.jsx
@@ -1,62 +1,58 @@
-import React, { useRef, useState, useLayoutEffect, useCallback } from "react";
-import ResizeObserver from "resize-observer-polyfill";
-import { motion, useScroll, useTransform, useSpring } from "framer-motion";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import "./Smoothscroll.css";
 import Slide1 from "../assets/images/slider-1.png";
 import Slide2 from "../assets/images/slider-2.png";
 import Slide3 from "../assets/images/slider-3.png";
 
+gsap.registerPlugin(ScrollTrigger);
+
 const SmoothScroll = () => {
-  const scrollRef = useRef(null);
-  const ghostRef = useRef(null);
-  const [scrollRange, setScrollRange] = useState(0);
-  const [viewportW, setViewportW] = useState(0);
+  const panels = useRef([]);
+  const panelsContainer = useRef();
 
-  useLayoutEffect(() => {
-    scrollRef && setScrollRange(scrollRef.current.scrollWidth);
-  }, [scrollRef]);
+  const createPanelsRefs = (panel, index) => {
+    panels.current[index] = panel;
+  };
 
-  const onResize = useCallback((entries) => {
-    for (let entry of entries) {
-      setViewportW(entry.contentRect.width);
-    }
+  useEffect(() => {
+    const totalPanels = panels.current.length;
+
+    gsap.to(panels.current, {
+      xPercent: -100 * (totalPanels - 1),
+      ease: "none",
+      scrollTrigger: {
+        trigger: panelsContainer.current,
+        pin: true,
+        scrub: 1,
+        snap: 1 / (totalPanels - 1),
+        // base vertical scrolling on how wide the container is so it feels more natural.
+        end: () => "+=" + panelsContainer.current.offsetWidth,
+      },
+    });
   }, []);
 
-  useLayoutEffect(() => {
-    const resizeObserver = new ResizeObserver((entries) => onResize(entries));
-    resizeObserver.observe(ghostRef.current);
-    return () => resizeObserver.disconnect();
-  }, [onResize]);
-
-  const { scrollYProgress } = useScroll();
-
-  const transform = useTransform(
-    scrollYProgress,
-    [0, 1],
-    [0, -scrollRange + viewportW]
-  );
-  const physics = { damping: 15, mass: 0.27, stiffness: 55 };
-  const spring = useSpring(transform, physics);
-  console.log(spring);
   return (
     <>
-      <div className="scroll-container">
-        <motion.section
-          ref={scrollRef}
-          style={{ x: spring }}
-          className="thumbnails-container"
-        >
-          <div className="thumbnails">
-            <div className="thumbnail" />
-            <div className="thumbnail" />
-            <div className="thumbnail" />
-            <div className="thumbnail" />
-            <div className="thumbnail" />
-            <div className="thumbnail" />
-          </div>
-        </motion.section>
+      <div className="container" ref={panelsContainer}>
+        <section className="panel red" ref={(e) => createPanelsRefs(e, 1)}>
+          <img src={Slide1} />
+        </section>
+        <section className="panel orange" ref={(e) => createPanelsRefs(e, 2)}>
+          <img src={Slide2} />
+        </section>
+        <section className="panel purple" ref={(e) => createPanelsRefs(e, 3)}>
+          <img src={Slide3} />
+        </section>
+        <section className="panel green" ref={(e) => createPanelsRefs(e, 4)}>
+          <img src={Slide1} />
+        </section>
+        <section className="panel gray" ref={(e) => createPanelsRefs(e, 5)}>
+          <img src={Slide2} />
+        </section>
       </div>
-      <div ref={ghostRef} style={{ height: scrollRange }} className="ghost" />
     </>
   );
 };
--- src/components/herobanner.jsx
+++ src/components/herobanner.jsx
@@ -61,6 +61,7 @@ function Herobanner() {
       <Parallaxstyletwo />
       <Institutionlayer />
       <Sectionbreaker />
+      <SmoothScroll />
     </div>
   );
 }
